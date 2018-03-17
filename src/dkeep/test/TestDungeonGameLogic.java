f0dc2a23bced995ea6aacf808a31a5b543c764e8
Xavi <pedro1998pinho@gmail.com>
2018-03-17 14:17:44 +0000

    Added extra class for keep tests

--- src/dkeep/test/TestDungeonGameLogic.java
+++ src/dkeep/test/TestDungeonGameLogic.java
@@ -13,8 +13,7 @@ import dkeep.logic.characters.Hero.key_t;
 import dkeep.logic.layout.Level.status_t;
 
 public class TestDungeonGameLogic {
-	/*
-	//Dungeon Tests
+
 	@Test
 	public void moveHeroIntoFreeCell() throws IOException, FileNotFoundException {
 		
@@ -69,7 +68,7 @@ public class TestDungeonGameLogic {
 		assertEquals('I',test3.getCurrentLevel().getMap()[3][0]);
 		assertEquals(status_t.ONGOING, test3.getCurrentLevel().getLevelStatus());
 	}
-	*/
+	
 	@Test
 	public void heroPicksUpKeyAndOpensDoors() throws IOException, FileNotFoundException {
 		
@@ -99,119 +98,6 @@ public class TestDungeonGameLogic {
 		test5.getCurrentLevel().updateLevel('a');
 		
 		assertEquals(status_t.WON, test5.getCurrentLevel().getLevelStatus());
-	}
-	
-	//Keep Tests
-	@Test 
-	public void heroKilledByOgre() throws IOException, FileNotFoundException {
-		
-		//Calls the constructor with "Rookie" but we never use the Guard, just for the constructor only
-		Game test = new Game(new ConsoleIO(), "Rookie", 3, 4);
-	
-		assertEquals(status_t.ONGOING, test.getCurrentLevel().getLevelStatus());
-		
-		test.getCurrentLevel().updateLevel('d');
-		test.getCurrentLevel().updateLevel('d');
-		
-		assertEquals(status_t.KILLED, test.getCurrentLevel().getLevelStatus());
-	}
-	
-	@Test 
-	public void heroPicksUpKey() throws IOException, FileNotFoundException {
-		
-		//Calls the constructor with "Rookie" but we never use the Guard, just for the constructor only
-		Game test = new Game(new ConsoleIO(), "Rookie", 3, 4);
-	
-		assertEquals('A', test.getCurrentLevel().getHero().getIcon());
-		
-		test.getCurrentLevel().updateLevel('s');
-		test.getCurrentLevel().updateLevel('s');
-		test.getCurrentLevel().updateLevel('s');
-		test.getCurrentLevel().updateLevel('d');
-		test.getCurrentLevel().updateLevel('d');
-		test.getCurrentLevel().updateLevel('d');
-		
-		assertEquals('K', test.getCurrentLevel().getHero().getIcon());
-		assertEquals(key_t.KEY, test.getCurrentLevel().getHero().getKey());
-	}
-	
-	@Test 
-	public void heroFailsToLeaveKeep() throws IOException, FileNotFoundException {
-		
-		//Calls the constructor with "Rookie" but we never use the Guard, just for the constructor only
-		Game test = new Game(new ConsoleIO(), "Rookie", 3, 4);
-	
-		assertEquals(status_t.ONGOING, test.getCurrentLevel().getLevelStatus());
-		assertEquals(key_t.NULL, test.getCurrentLevel().getHero().getKey());
-		
-		test.getCurrentLevel().updateLevel('s');
-		test.getCurrentLevel().updateLevel('s');
-		test.getCurrentLevel().updateLevel('s');
-		test.getCurrentLevel().updateLevel('a');
-		
-		assertEquals(key_t.NULL, test.getCurrentLevel().getHero().getKey());
-		assertEquals(status_t.ONGOING, test.getCurrentLevel().getLevelStatus());
-	}
-	
-	@Test 
-	public void heroOpensDoorWithKey() throws IOException, FileNotFoundException {
-		
-		//Calls the constructor with "Rookie" but we never use the Guard, just for the constructor only
-		Game test = new Game(new ConsoleIO(), "Rookie", 3, 4);
-	
-		assertEquals(status_t.ONGOING, test.getCurrentLevel().getLevelStatus());
-		
-		test.getCurrentLevel().updateLevel('s');
-		test.getCurrentLevel().updateLevel('s');
-		test.getCurrentLevel().updateLevel('s');
-		test.getCurrentLevel().updateLevel('d');
-		test.getCurrentLevel().updateLevel('d');
-		test.getCurrentLevel().updateLevel('d');
-		
-		//Got the key
-		assertEquals('K', test.getCurrentLevel().getHero().getIcon());
-		assertEquals(key_t.KEY, test.getCurrentLevel().getHero().getKey());
-		
-		test.getCurrentLevel().updateLevel('a');
-		test.getCurrentLevel().updateLevel('a');
-		test.getCurrentLevel().updateLevel('a');
-		test.getCurrentLevel().updateLevel('a');
-		
-		assertEquals('S',test.getCurrentLevel().getMap()[4][0]);
-		assertEquals(key_t.UNLOCKED, test.getCurrentLevel().getHero().getKey());
-		assertEquals(status_t.ONGOING, test.getCurrentLevel().getLevelStatus());
-	}
-	
-	@Test 
-	public void heroWinsTheGame() throws IOException, FileNotFoundException {
-		
-		//Calls the constructor with "Rookie" but we never use the Guard, just for the constructor only
-		Game test = new Game(new ConsoleIO(), "Rookie", 3, 4);
-	
-		assertEquals(status_t.ONGOING, test.getCurrentLevel().getLevelStatus());
-		
-		test.getCurrentLevel().updateLevel('s');
-		test.getCurrentLevel().updateLevel('s');
-		test.getCurrentLevel().updateLevel('s');
-		test.getCurrentLevel().updateLevel('d');
-		test.getCurrentLevel().updateLevel('d');
-		test.getCurrentLevel().updateLevel('d');
-		
-		//Got the key
-		assertEquals('K', test.getCurrentLevel().getHero().getIcon());
-		assertEquals(key_t.KEY, test.getCurrentLevel().getHero().getKey());
-		
-		test.getCurrentLevel().updateLevel('a');
-		test.getCurrentLevel().updateLevel('a');
-		test.getCurrentLevel().updateLevel('a');
-		test.getCurrentLevel().updateLevel('a');
-		
-		assertEquals('S',test.getCurrentLevel().getMap()[4][0]);
-		assertEquals(key_t.UNLOCKED, test.getCurrentLevel().getHero().getKey());
-		assertEquals(status_t.ONGOING, test.getCurrentLevel().getLevelStatus());
-		
-		test.getCurrentLevel().updateLevel('a');
-		assertEquals(status_t.WON, test.getCurrentLevel().getLevelStatus());
 	}
 	
 }
---
+++ src/dkeep/test/TestKeepGameLogic.java
@@ -0,0 +1,129 @@
+package dkeep.test;
+
+import static org.junit.Assert.*;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.junit.Test;
+
+import dkeep.cli.Game;
+import dkeep.io.ConsoleIO;
+import dkeep.logic.characters.Hero.key_t;
+import dkeep.logic.layout.Level.status_t;
+
+public class TestKeepGameLogic {
+
+	@Test 
+	public void heroKilledByOgre() throws IOException, FileNotFoundException {
+		
+		//Calls the constructor with "Rookie" but we never use the Guard, just for the constructor only
+		Game test = new Game(new ConsoleIO(), "Rookie", 3, 4);
+	
+		assertEquals(status_t.ONGOING, test.getCurrentLevel().getLevelStatus());
+		
+		test.getCurrentLevel().updateLevel('d');
+		test.getCurrentLevel().updateLevel('d');
+		
+		assertEquals(status_t.KILLED, test.getCurrentLevel().getLevelStatus());
+	}
+	
+	@Test 
+	public void heroPicksUpKey() throws IOException, FileNotFoundException {
+		
+		//Calls the constructor with "Rookie" but we never use the Guard, just for the constructor only
+		Game test = new Game(new ConsoleIO(), "Rookie", 3, 4);
+	
+		assertEquals('A', test.getCurrentLevel().getHero().getIcon());
+		
+		test.getCurrentLevel().updateLevel('s');
+		test.getCurrentLevel().updateLevel('s');
+		test.getCurrentLevel().updateLevel('s');
+		test.getCurrentLevel().updateLevel('d');
+		test.getCurrentLevel().updateLevel('d');
+		test.getCurrentLevel().updateLevel('d');
+		
+		assertEquals('K', test.getCurrentLevel().getHero().getIcon());
+		assertEquals(key_t.KEY, test.getCurrentLevel().getHero().getKey());
+	}
+	
+	@Test 
+	public void heroFailsToLeaveKeep() throws IOException, FileNotFoundException {
+		
+		//Calls the constructor with "Rookie" but we never use the Guard, just for the constructor only
+		Game test = new Game(new ConsoleIO(), "Rookie", 3, 4);
+	
+		assertEquals(status_t.ONGOING, test.getCurrentLevel().getLevelStatus());
+		assertEquals(key_t.NULL, test.getCurrentLevel().getHero().getKey());
+		
+		test.getCurrentLevel().updateLevel('s');
+		test.getCurrentLevel().updateLevel('s');
+		test.getCurrentLevel().updateLevel('s');
+		test.getCurrentLevel().updateLevel('a');
+		
+		assertEquals(key_t.NULL, test.getCurrentLevel().getHero().getKey());
+		assertEquals(status_t.ONGOING, test.getCurrentLevel().getLevelStatus());
+	}
+	
+	@Test 
+	public void heroOpensDoorWithKey() throws IOException, FileNotFoundException {
+		
+		//Calls the constructor with "Rookie" but we never use the Guard, just for the constructor only
+		Game test = new Game(new ConsoleIO(), "Rookie", 3, 4);
+	
+		assertEquals(status_t.ONGOING, test.getCurrentLevel().getLevelStatus());
+		
+		test.getCurrentLevel().updateLevel('s');
+		test.getCurrentLevel().updateLevel('s');
+		test.getCurrentLevel().updateLevel('s');
+		test.getCurrentLevel().updateLevel('d');
+		test.getCurrentLevel().updateLevel('d');
+		test.getCurrentLevel().updateLevel('d');
+		
+		//Got the key
+		assertEquals('K', test.getCurrentLevel().getHero().getIcon());
+		assertEquals(key_t.KEY, test.getCurrentLevel().getHero().getKey());
+		
+		test.getCurrentLevel().updateLevel('a');
+		test.getCurrentLevel().updateLevel('a');
+		test.getCurrentLevel().updateLevel('a');
+		test.getCurrentLevel().updateLevel('a');
+		
+		assertEquals('S',test.getCurrentLevel().getMap()[4][0]);
+		assertEquals(key_t.UNLOCKED, test.getCurrentLevel().getHero().getKey());
+		assertEquals(status_t.ONGOING, test.getCurrentLevel().getLevelStatus());
+	}
+	
+	@Test 
+	public void heroWinsTheGame() throws IOException, FileNotFoundException {
+		
+		//Calls the constructor with "Rookie" but we never use the Guard, just for the constructor only
+		Game test = new Game(new ConsoleIO(), "Rookie", 3, 4);
+	
+		assertEquals(status_t.ONGOING, test.getCurrentLevel().getLevelStatus());
+		
+		test.getCurrentLevel().updateLevel('s');
+		test.getCurrentLevel().updateLevel('s');
+		test.getCurrentLevel().updateLevel('s');
+		test.getCurrentLevel().updateLevel('d');
+		test.getCurrentLevel().updateLevel('d');
+		test.getCurrentLevel().updateLevel('d');
+		
+		//Got the key
+		assertEquals('K', test.getCurrentLevel().getHero().getIcon());
+		assertEquals(key_t.KEY, test.getCurrentLevel().getHero().getKey());
+		
+		test.getCurrentLevel().updateLevel('a');
+		test.getCurrentLevel().updateLevel('a');
+		test.getCurrentLevel().updateLevel('a');
+		test.getCurrentLevel().updateLevel('a');
+		
+		assertEquals('S',test.getCurrentLevel().getMap()[4][0]);
+		assertEquals(key_t.UNLOCKED, test.getCurrentLevel().getHero().getKey());
+		assertEquals(status_t.ONGOING, test.getCurrentLevel().getLevelStatus());
+		
+		test.getCurrentLevel().updateLevel('a');
+		assertEquals(status_t.WON, test.getCurrentLevel().getLevelStatus());
+	}
+
+}
